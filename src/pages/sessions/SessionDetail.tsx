88e3fe6ec29ca386c35f8462d8a8b5b10d0b4afe
pukechamp <extremexis1@outlook.com>
2025-11-14 17:53:30 -0800

    Added a second adress field tp create session and session detail

--- src/pages/sessions/SessionDetail.tsx
+++ src/pages/sessions/SessionDetail.tsx
@@ -243,6 +243,9 @@ const SessionDetail: React.FC = () => {
                 <Descriptions.Item label={<Space><EnvironmentOutlined /> Location</Space>}>
                   {session.location || 'Not specified'}
                 </Descriptions.Item>
+                <Descriptions.Item label={<Space><EnvironmentOutlined /> Address</Space>}>
+                  {session.address || 'Not specified'}
+                </Descriptions.Item>
                 <Descriptions.Item label={<Space><CustomerServiceOutlined /> Genre</Space>}>
                   <Tag>{session.genre || 'Any'}</Tag>
                 </Descriptions.Item>
